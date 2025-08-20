b1943ef0f9d7f9400c8685370b368b07e4323dee
nibilin33 <nibilin33@gmail.com>
2025-08-20 19:52:39 +0800

    fix: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 // tailwind.config.js
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",   
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/liquid-tailwind-react/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/liquid-tailwind-react/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -14,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
